720a7477106df9a07f903cf96a61d1dc3542db30
Onofre Martorell <>
2017-03-03 12:12:38 +0100

    Changes+gitignore

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -51,11 +51,11 @@ figure; imshow(I); figure; imshow(uint8(I2));
 %% 2. Affine Rectification
 
 
-% choose the image points
-I=imread('Data/0000_s.png');
+% Choose the image points
+I = imread('Data/0000_s.png');
 A = load('Data/0000_s_info_lines.txt');
 
-% indices of lines
+% Indices of lines
 i = 424;
 p1 = [A(i,1) A(i,2) 1]';
 p2 = [A(i,3) A(i,4) 1]';
@@ -70,12 +70,16 @@ p7 = [A(i,1) A(i,2) 1]';
 p8 = [A(i,3) A(i,4) 1]';
 
 % ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
+% Done
+l1 = cross(p1, p2);
+l2 = cross(p3, p4);
+l3 = cross(p5, p6);
+l4 = cross(p7, p8);
 
-
-% show the chosen lines in the image
+% Show the chosen lines in the image
 figure;imshow(I);
 hold on;
-t=1:0.1:1000;
+t = 1:0.1:1000;
 plot(t, -(l1(1)*t + l1(3)) / l1(2), 'y');
 plot(t, -(l2(1)*t + l2(3)) / l2(2), 'y');
 plot(t, -(l3(1)*t + l3(3)) / l3(2), 'y');
@@ -83,15 +87,30 @@ plot(t, -(l4(1)*t + l4(3)) / l4(2), 'y');
 
 % ToDo: compute the homography that affinely rectifies the image
 
+% Vanishing points
+v1 = cross(l1, l2);
+v2 = cross(l3, l4);
+
+% Line at infinity
+l_inf = cross(v1, v2);
+
+H = [1 0 0; 0 1 0; l_inf];
 I2 = apply_H(I, H);
 figure; imshow(uint8(I2));
 
 % ToDo: compute the transformed lines lr1, lr2, lr3, lr4
+%  l'= H^-T*l
+
+H_inv = inv(H)';
+lr1 = H_inv*l1;
+lr2 = H_inv*l2;
+lr3 = H_inv*l3;
+lr4 = H_inv*l4;
 
 % show the transformed lines in the transformed image
 figure;imshow(uint8(I2));
 hold on;
-t=1:0.1:1000;
+t = 1:0.1:1000;
 plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
 plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'y');
 plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'y');
@@ -100,6 +119,19 @@ plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
 % ToDo: to evaluate the results, compute the angle between the different pair 
 % of lines before and after the image transformation
 
+% Angle between line 1 and line 2 before rectification
+normal_l1 = [l1(1)/l1(3) l1(2)/l1(3)];
+normal_l2 = [l2(1)/l2(3) l2(2)/l2(3)];
+norm_l1 = sqrt(dot(normal_l1, normal_l1));
+norm_l2 = sqrt(dot(normal_l2, normal_l2));
+angle_l1_l2 = acos(dot(normal_l1, normal_l2)/(norm_l1*norm_l2));
+
+% Angle between line 1 and line 2 after rectification
+normal_lr1 = [lr1(1)/lr1(3) lr1(2)/lr1(3)];
+normal_lr2 = [lr2(1)/lr2(3) lr2(2)/lr2(3)];
+norm_lr1 = sqrt(dot(normal_lr1, normal_lr1));
+norm_lr2 = sqrt(dot(normal_lr2, normal_lr2));
+angle_lr1_lr2 = acos(dot(normal_lr1, normal_lr2)/(norm_lr1*norm_lr2));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 3. Metric Rectification
