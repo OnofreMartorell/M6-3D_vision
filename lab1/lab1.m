e9ccefd055c07435284b477192fe57d5373b75f2
Lidia Talavera <lidia_tama@hotmail.com>
2017-03-04 22:18:58 +0100

    metric rectification, not finished yet

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -99,7 +99,7 @@ v2 = cross(l3, l4);
 % Line at infinity
 l_inf = cross(v1, v2);
 
-H = [1 0 0; 0 1 0; l_inf];
+H = [1 0 0; 0 1 0; l_inf'];
 I2 = apply_H(I, H);
 figure; imshow(uint8(I2));
 
@@ -164,6 +164,119 @@ angle_lr3_lr4 = acos(dot(normal_lr3, normal_lr4)/(norm_lr3*norm_lr4));
 %       the metric rectification) with the chosen lines printed on it.
 %       Compute also the angles between the pair of lines before and after
 %       rectification.
+
+Hm = eye(3);
+% Compute the lines l1, m1, l2, m2, that pass through the different pairs of points
+l1 = lr1;
+m1 = lr2;
+l2 = lr3;
+m2 = lr4;
+
+% Choose the image points
+I = imread('Data/0000_s.png');
+A = load('Data/0000_s_info_lines.txt');
+
+% Indices of lines
+i = 424;
+p1 = [A(i,1) A(i,2) 1]';
+p2 = [A(i,3) A(i,4) 1]';
+i = 712;
+p3 = [A(i,1) A(i,2) 1]';
+p4 = [A(i,3) A(i,4) 1]';
+i = 565;
+p5 = [A(i,1) A(i,2) 1]';
+p6 = [A(i,3) A(i,4) 1]';
+i = 240;
+p7 = [A(i,1) A(i,2) 1]';
+p8 = [A(i,3) A(i,4) 1]';
+
+% ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
+% Done
+l1 = cross(p1, p2);
+m1 = cross(p3, p4);
+l2 = cross(p5, p6);
+m2 = cross(p7, p8);
+
+% Show the chosen lines in the image
+figure;imshow(I);
+hold on;
+t = 1:0.1:1000;
+plot(t, -(l1(1)*t + l1(3)) / l1(2), 'y');
+plot(t, -(m1(1)*t + m1(3)) / m1(2), 'y');
+plot(t, -(l2(1)*t + l2(3)) / l2(2), 'g');
+plot(t, -(m2(1)*t + m2(3)) / m2(2), 'g');
+
+% Set up the constraints from the orthogonal line pairs
+M = [l1(1)*m1(1), l1(1)*m1(2) + l1(2)*m1(1), l1(2)*m1(2);
+     l2(1)*m2(1), l2(1)*m2(2) + l2(2)*m2(1), l2(2)*m2(2)];
+ 
+% Find s and S from the null space of the constraints matrix
+s = null(M);
+S = [s(1) s(2); s(2) s(3)];
+
+% [R,p] = chol(A) for positive definite A, produces an upper triangular matrix
+% R from the diagonal and upper triangle of matrix A, satisfying the
+% equation R'*R=A and p is zero. If A is not positive definite, then p is
+% a positive integer and MATLAB does not generate an error. When A is full,
+% R is an upper triangular matrix of order q=p-1 such that R'*R=A(1:q,1:q).
+% When A is sparse, R is an upper triangular matrix of size q-by-n so that
+% the L-shaped region of the first q rows and first q columns of R'*R agree
+% with those of A.
+
+[K,p] = chol(S,'upper');
+Hm(1:2,1:2) = K;
+Hm = inv(Hm);
+
+% Apply homography
+I3 = apply_H(I2, H_m);
+
+% Compute the transformed lines lr1, lr2, lr3, lr4
+%  l'= H^-T*l
+H_inv = inv(H)';
+l1mr = H_m*l1;
+m1mr = H_m*m1;
+l2mr = H_m*l2;
+m2mr = H_m*m2;
+
+% Show the transformed lines in the transformed image
+figure;imshow(uint8(I2));
+hold on;
+t = 1:0.1:1000;
+plot(t, -(l1mr(1)*t + l1mr(3)) / l1mr(2), 'y');
+plot(t, -(m1mr(1)*t + m1mr(3)) / m1mr(2), 'y');
+plot(t, -(l2mr(1)*t + l2mr(3)) / l2mr(2), 'g');
+plot(t, -(m2mr(1)*t + m2mr(3)) / m2mr(2), 'g');
+
+% Evaluate the results, compute the angle between the different pair 
+% of lines before and after the image transformation
+
+% Angle between line 1 and line 2 before rectification
+normal_l1 = [l1(1)/l1(3) l1(2)/l1(3)];
+normal_m1 = [m1(1)/m1(3) m1(2)/m1(3)];
+norm_l1 = sqrt(dot(normal_l1, normal_l1));
+norm_m1 = sqrt(dot(normal_m1, normal_m1));
+angle_l1_m1 = acos(dot(normal_l1, normal_m1)/(norm_l1*norm_m1));
+
+% Angle between line 1 and line 2 after rectification
+normal_l1mr = [l1mr(1)/l1mr(3) l1mr(2)/l1mr(3)];
+normal_m1mr = [m1mr(1)/m1mr(3) m1mr(2)/m1mr(3)];
+norm_l1mr = sqrt(dot(normal_l1mr, normal_l1mr));
+norm_m1mr = sqrt(dot(normal_m1mr, normal_m1mr));
+angle_l1mr_m1mr = acos(dot(normal_l1mr, normal_m1mr)/(norm_l1mr*norm_m1mr));
+
+% Angle between line 3 and line 4 before rectification
+normal_l2 = [l2(1)/l2(3) l2(2)/l2(3)];
+normal_m2 = [m2(1)/m2(3) m2(2)/m2(3)];
+norm_l2 = sqrt(dot(normal_l2, normal_l2));
+norm_m2 = sqrt(dot(normal_m2, normal_m2));
+angle_l2_m2 = acos(dot(normal_l2, normal_m2)/(norm_l2*norm_m2));
+
+% Angle between line 3 and line 4 after rectification
+normal_l2mr = [l2mr(1)/l2mr(3) l2mr(2)/l2mr(3)];
+normal_m2mr = [m2mr(1)/m2mr(3) m2mr(2)/m2mr(3)];
+norm_l2mr = sqrt(dot(normal_l2mr, normal_l2mr));
+norm_m2mr = sqrt(dot(normal_m2mr, normal_m2mr));
+angle_l2mr_m2mr = acos(dot(normal_l2mr, normal_m2mr)/(norm_l2mr*norm_m2mr));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 4. OPTIONAL: Metric Rectification in a single step
