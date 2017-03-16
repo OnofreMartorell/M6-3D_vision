17d7db1195f3fb8ea173e67ce96caedfcb3bdf29
Lidia Talavera <lidia_tama@hotmail.com>
2017-03-16 17:39:54 +0100

    DLT and Ransac

--- lab2/homography2d.m
+++ lab2/homography2d.m
@@ -1,20 +1,22 @@
-function H = homography2d(x1, x2)
-
-sizeX1 = size(x1);
-A=zeros(2*sizeX1(2),9);
-
-for i=1:2:sizeX1(2)
+function H = homography2d(x1, x2)    
+    % Normalization
+    [x1n,T1] = normPts(x1);
+    [x2n,T2] = normPts(x2);
     
-A(i:i+1,:) = [0 0 0 -x2(3,i)*x1(1,i) -x2(3,i)*x1(2,i) -x2(3,i)*x1(3,i) x2(2,i)*x1(1,i) x2(2,i)*x1(2,i) x2(2,i)*x1(3,i);
-    x2(3,i)*x1(1,i) x2(3,i)*x1(2,i) x2(3,i)*x1(3,i) 0 0 0 -x2(1,i)*x1(1,i) -x2(1,i)*x1(2,i) -x2(1,i)*x1(3,i)];
-end
+    % Compute DLT
+    sizeX1 = size(x1n);
+    A = zeros(2*sizeX1(2),9);
 
-%     A(i,:) = [0 0 0 -x2(i,3)*x1(i,1) -x2(i,3)*x1(i,2) -x2(i,3)*x1(i,3) x2(i,2)*x1(i,1) x2(i,2)*x1(i,2) x2(i,2)*x1(i,3);
-%     x2(i,3)*x1(i,1) x2(i,3)*x1(i,2) x2(i,3)*x1(i,3) 0 0 0 -x2(i,1)*x1(i,1) -x2(i,1)*x1(i,2) -x2(i,1)*x1(i,3)];
+    for i=1:2:sizeX1(2)
 
-[U,D,V] = svd(A);
-H_vector = V(:,end);
-
-H=reshape(H_vector,[3,3])';
-
+    A(i:i+1,:) = [0 0 0 -x2n(3,i)*x1n(1,i) -x2n(3,i)*x1n(2,i) -x2n(3,i)*x1n(3,i) x2n(2,i)*x1n(1,i) x2n(2,i)*x1n(2,i) x2n(2,i)*x1n(3,i);
+        x2n(3,i)*x1n(1,i) x2n(3,i)*x1n(2,i) x2n(3,i)*x1n(3,i) 0 0 0 -x2n(1,i)*x1n(1,i) -x2n(1,i)*x1n(2,i) -x2n(1,i)*x1n(3,i)];
+    end
+    
+    [U,D,V] = svd(A);
+    H_vector = V(:,end);
+    H = reshape(H_vector,[3,3])';
+    
+    % Desnormalization
+    H = pinv(T2) * H * T1 ;
 end
---
+++ lab2/normPts.m
@@ -0,0 +1,27 @@
+function [NewPts,T] = normPts(points)
+
+    finiteind = find(abs(points(3,:)) > eps);
+    
+    if length(finiteind) ~= size(points,2)
+        warning('Some points are at infinity');
+    end
+
+    points(1,finiteind) = points(1,finiteind)./points(3,finiteind);
+    points(2,finiteind) = points(2,finiteind)./points(3,finiteind);
+    points(3,finiteind) = 1;
+
+    c = mean(points(1:2,finiteind)')';            % Centroid of finite points
+    newp(1,finiteind) = points(1,finiteind)-c(1); % Shift origin to centroid.
+    newp(2,finiteind) = points(2,finiteind)-c(2);
+
+    dist = sqrt(newp(1,finiteind).^2 + newp(2,finiteind).^2);
+    meandist = mean(dist(:));  % Ensure dist is a column vector for Octave 3.0.1
+
+    scale = sqrt(2)/meandist;
+
+    T = [scale   0   -scale*c(1)
+         0     scale -scale*c(2)
+         0       0      1      ];
+
+    NewPts = T*points;
+end
