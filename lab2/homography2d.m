1c7cc2e456895625753dc34106a7924975dc0027
Lidia Talavera <lidia_tama@hotmail.com>
2017-03-16 15:49:32 +0100

    A

--- lab2/homography2d.m
+++ lab2/homography2d.m
@@ -1,12 +1,16 @@
 function H = homography2d(x1, x2)
 
 sizeX1 = size(x1);
-A=zeros(2*sizeX1(1),9);
+A=zeros(2*sizeX1(2),9);
 
-for i=1:2:sizeX1(1)
-    A(i,:) = [0 0 0 -x2(i,3)*x1(i,1) -x2(i,3)*x1(i,2) -x2(i,3)*x1(i,3) x2(i,2)*x1(i,1) x2(i,2)*x1(i,2) x2(i,2)*x1(i,3);
-    x2(i,3)*x1(i,1) x2(i,3)*x1(i,2) x2(i,3)*x1(i,3) 0 0 0 -x2(i,1)*x1(i,1) -x2(i,1)*x1(i,2) -x2(i,1)*x1(i,3)];
+for i=1:2:sizeX1(2)
+    
+A(i:i+1,:) = [0 0 0 -x2(3,i)*x1(1,i) -x2(3,i)*x1(2,i) -x2(3,i)*x1(3,i) x2(2,i)*x1(1,i) x2(2,i)*x1(2,i) x2(2,i)*x1(3,i);
+    x2(3,i)*x1(1,i) x2(3,i)*x1(2,i) x2(3,i)*x1(3,i) 0 0 0 -x2(1,i)*x1(1,i) -x2(1,i)*x1(2,i) -x2(1,i)*x1(3,i)];
 end
+
+%     A(i,:) = [0 0 0 -x2(i,3)*x1(i,1) -x2(i,3)*x1(i,2) -x2(i,3)*x1(i,3) x2(i,2)*x1(i,1) x2(i,2)*x1(i,2) x2(i,2)*x1(i,3);
+%     x2(i,3)*x1(i,1) x2(i,3)*x1(i,2) x2(i,3)*x1(i,3) 0 0 0 -x2(i,1)*x1(i,1) -x2(i,1)*x1(i,2) -x2(i,1)*x1(i,3)];
 
 [U,D,V] = svd(A);
 H_vector = V(:,end);
