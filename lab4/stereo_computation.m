4c67a45d3676e350cc9658e6d0e24393e421ee27
OnofreMartorell <onofremartorelln@gmail.com>
2017-04-03 12:10:47 +0200

    Making the code of stereo clean

--- lab4/stereo_computation.m
+++ lab4/stereo_computation.m
@@ -1,22 +1,23 @@
-function disparity = stereo_computation(left_im,right_im,min_disp,max_disp,w_size,matching_cost)
+function disparity = stereo_computation(left_im, right_im, min_disp, max_disp, w_size, matching_cost)
 
 [row, col] = size(left_im);
-
+length_side_window = ceil(w_size/2) - 1;
 % Intermediate matrix to store the result for each pixel depending on the maximum diparity 
-pixel_correlation = zeros(row,col,1+max_disp);
-
-for i = ceil(w_size/2):1:row-ceil(w_size/2)
-    for j = (max_disp/2)+ceil(w_size/2):1:col-(max_disp/2)-ceil(w_size/2)
-        block_left = left_im((i - (ceil(w_size/2)-1)):(i + (ceil(w_size/2)-1)),...
-            (j - (ceil(w_size/2)-1)):(j + (ceil(w_size/2)-1)));
-        
+pixel_correlation = zeros(row, col, 1 + max_disp);
+% For each point in the left image
+for i = ceil(w_size/2):row - ceil(w_size/2)
+    for j = (max_disp/2) + ceil(w_size/2):col - (max_disp/2) - ceil(w_size/2)
+        % Window on the left image
+        block_left = left_im((i - length_side_window):(i + length_side_window),...
+            (j - length_side_window):(j + length_side_window));
+       % Find the ssd cost with windows at the other image 
        ind_disp = 1; 
-       for k =j-(max_disp/2):1:j+(max_disp/2)           
-           block_right = right_im((i - (ceil(w_size/2)-1)):(i + (ceil(w_size/2)-1)),...
-               (k - (ceil(w_size/2)-1)):(k + (ceil(w_size/2)-1)));
+       for k = j - (max_disp/2):j + (max_disp/2)           
+           block_right = right_im((i - length_side_window):(i + length_side_window),...
+               (k - length_side_window):(k + length_side_window));
           
-           if strcmp(matching_cost,'SSD')
-                pixel_correlation(i,j,ind_disp) = sum(sum((block_left - block_right).^2));
+           if strcmp(matching_cost, 'SSD')
+                pixel_correlation(i, j, ind_disp) = sum(sum((block_left - block_right).^2));
 %            elseif strcmp(matching_cost,'NCC')
 %                %pixel_correlation(i,j,ind_disp) = ;
            end
@@ -25,7 +26,7 @@ for i = ceil(w_size/2):1:row-ceil(w_size/2)
     end
 end
 
-[~,ind]=min(pixel_correlation,[],3);
-disparity = ind-1;
+[~, ind] = min(pixel_correlation, [], 3);
+disparity = ind - 1;
 end
 
